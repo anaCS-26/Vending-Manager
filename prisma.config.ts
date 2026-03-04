3dd8214451bef2280663076233cbf84588b15aa5
anaCS-26 <79747634+anaCS-26@users.noreply.github.com>
2026-03-03 23:14:24 -0500

    Upgrade to Prisma 7 and migrate configuration to prisma.config.ts

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@prisma/config'
+
+export default defineConfig({
+    earlyAccess: true,
+    datasource: {
+        url: process.env.DATABASE_URL,
+        directUrl: process.env.DIRECT_URL,
+    },
+})
